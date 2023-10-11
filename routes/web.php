3cd502872125525b95ef950f897e05f2b99b2800
syauqitaqiyuddin <ahmadsyauqi150@gmail.com>
2023-10-11 09:11:10 +0700

    ora yakin

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProductController as Controller;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,7 +14,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', function () {return view('welcome');});
+
 Route::resource('products', Controller::class);
